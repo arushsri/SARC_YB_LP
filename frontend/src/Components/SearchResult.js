3df5f5fdf48240318a25d45e1e681a2291cc2f31
arushsri <166750119+arushsri@users.noreply.github.com>
2025-06-09 06:27:20 +0530

    Update SearchResult.js

--- frontend/src/Components/SearchResult.js
+++ frontend/src/Components/SearchResult.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import './SearchResult.css'
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
