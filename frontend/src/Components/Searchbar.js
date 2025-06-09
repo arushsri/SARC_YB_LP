500f5dd6f73d17e8db7f470b15388ad7b11e2628
arushsri <166750119+arushsri@users.noreply.github.com>
2025-06-09 06:30:54 +0530

    Update Searchbar.js

--- frontend/src/Components/Searchbar.js
+++ frontend/src/Components/Searchbar.js
@@ -4,45 +4,44 @@ import SearchContext from '../Contexts/SearchContext'
 import '../FormInputs.css'
 
 export default function Searchbar() {
-    const list = useContext(SearchContext);
+    const { input, updateResults } = useContext(SearchContext);
 
     useEffect(() => {
-        const delayDebounceFn = setTimeout(() => {
-          if (list.input) {
-            axios
-              .post('https://yearbook.sarc-iitb.org/api/search/search/', {
-                query: {
-                  multi_match: {
-                    fields: ['name', 'hostel', 'department', 'degree', 'program'],
-                    query: list.input,
-                    fuzziness: 'AUTO',
-                    type: 'best_fields',
-                  },
-                },
-              })
-              .then(response => {
-                const foundUsers = response.data.hits.hits.map(hit => hit._source);
-                const mappedUsers = foundUsers.map(user => ({
-                  id: user.id,
-                  display: user.name,
-                  department: user.department,
-                  profile_image: user.profile_image,
-                  hostel: user.hostel,
-                  is_ib: user.is_ib,
-                }));
-                list.updateResults(mappedUsers);
-              })
-              .catch(error => {
-                console.error('Error searching users:', error);
-              });
-          } else {
-            list.updateResults([]); // Clear users when search term is empty
-          }
-        }, 200); //ye dal diya...isse hi ab constant 6 nhi dikha raha
-        //matlb yadi elastic search ke views se ):^ hata denge to bhi sayad ye nhi hoga
+      const delayDebounceFn = setTimeout(() => {
+        if (input) {
+          axios.post('https://yearbook.sarc-iitb.org/api/search/search/', {
+            query: {
+              multi_match: {
+                fields: ['name', 'hostel', 'department', 'degree', 'program'],
+                query: input,
+                fuzziness: 'AUTO',
+                type: 'best_fields',
+              },
+            },
+          })
+          .then(response => {
+            const foundUsers = response.data.hits.hits.map(hit => hit._source);
+            const mappedUsers = foundUsers.map(user => ({
+              id: user.id,
+              display: user.name,
+              department: user.department,
+              profile_image: user.profile_image,
+              hostel: user.hostel,
+              is_ib: user.is_ib,
+            }));
+            updateResults(mappedUsers);
+          })
+          .catch(error => {
+            console.error('Error searching users:', error);
+          });
+        } else {
+          updateResults([]); // Clear users when search term is empty
+        }
+      }, 200);
     
-        return () => clearTimeout(delayDebounceFn);
-      }, [list.input]);
+      return () => clearTimeout(delayDebounceFn);
+    }, [input, updateResults]);
+
 
     const handleChange = (value) => {
         list.setInput(value);
